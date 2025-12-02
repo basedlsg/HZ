a5c8ddb54a884182a65aca0edbb3c42cef9ae43b
Hotzones Deploy <deployment@hotzones.app>
2025-12-02 17:23:52 +0800

    Remove deprecated Next.js config options

--- next.config.ts
+++ next.config.ts
@@ -5,13 +5,6 @@ const nextConfig = {
       bodySizeLimit: '50mb',
     },
   },
-  // Increase API route body size limit
-  serverRuntimeConfig: {
-    // Will only be available on the server side
-  },
-  publicRuntimeConfig: {
-    // Will be available on both server and client
-  },
 };
 
 export default nextConfig;
